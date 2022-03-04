e479383f63372674144302f7f6f13dde6e5e35ba
Rafael <rafaelmassaro9@gmail.com>
2022-03-04 20:32:30 -0300

    Implemented create and authenticate user funcionality

---
+++ backend/src/routes.ts
@@ -0,0 +1,15 @@
+import { Router} from 'express';
+
+import { AuthUserController } from './controllers/user/AuthUserController';
+import { CreateUserController } from './controllers/user/CreateUserController'
+import { DetailUserController } from './controllers/user/DetailUserController';
+
+import { isAuthenticated } from './middlewares/isAuthenticated'
+
+const router = Router();
+
+router.post('/users', new CreateUserController().handle)
+router.post('/session', new AuthUserController().handle)
+router.get('/me', isAuthenticated, new DetailUserController().handle)
+
+export {router};
---
+++ backend/src/server.ts
@@ -0,0 +1,26 @@
+import express, { Request, Response, NextFunction, json } from 'express';
+import "express-async-errors";
+import cors from 'cors';
+
+import { router } from './routes';
+
+const app = express();
+app.use(json());
+app.use(cors());
+
+app.use(router);
+
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+    if(error instanceof Error){
+        return res.status(400).json({
+            error: error.message
+        })
+    }
+
+    return res.status(500).json({
+        status: error,
+        message: "Internal server error!"
+    })
+})
+
+app.listen(3333, () => console.log("Server Running!"));
