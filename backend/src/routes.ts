362a9b5d60c2441f276eac9790cceb0e0eeb407f
Rafael <rafaelmassaro9@gmail.com>
2022-03-05 19:18:13 -0300

    Created Categories and Products funcionalities

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,15 +1,28 @@
 import { Router} from 'express';
+import multer from 'multer';
 
 import { AuthUserController } from './controllers/user/AuthUserController';
 import { CreateUserController } from './controllers/user/CreateUserController'
 import { DetailUserController } from './controllers/user/DetailUserController';
+import { CreateCategoryController } from './controllers/category/CreateCategoryController';
+import { ListCategoryController } from './controllers/category/ListCategoryController';
+import { CreateProductController } from './controllers/product/CreateProductController';
 
 import { isAuthenticated } from './middlewares/isAuthenticated'
 
+import uploadConfig from './config/multer'
+
 const router = Router();
+
+const upload = multer(uploadConfig.upload("./tmp"));
 
 router.post('/users', new CreateUserController().handle)
 router.post('/session', new AuthUserController().handle)
 router.get('/me', isAuthenticated, new DetailUserController().handle)
+
+router.post('/category', isAuthenticated, new CreateCategoryController().handle)
+router.get('/category', isAuthenticated, new ListCategoryController().handle)
+
+router.post('/product', isAuthenticated, upload.single('file'), new CreateProductController().handle)
 
 export {router};
