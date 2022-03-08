765c3e6da9c3b74df7df67b1bab3932b7728a20b
Rafael <rafaelmassaro9@gmail.com>
2022-03-07 22:17:56 -0300

    Finalize

---
+++ backend/src/controllers/order/ListOrderController.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from "express";
+
+import { ListOrderService } from "../../services/order/ListOrderService";
+
+class ListOrderController{
+    async handle(req: Request, res: Response){
+
+        const listOrder = new ListOrderService()
+
+        const order = await listOrder.execute()
+
+        return res.json(order)
+
+    }
+}
+
+export {ListOrderController}
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -12,6 +12,10 @@ import { CreateOrderController } from './controllers/order/CreateOrderController
 import { RemoveOrderController } from './controllers/order/RemoveOrderController';
 import { AddItemController } from './controllers/order/AddItemController';
 import { RemoveItemController } from './controllers/order/RemoveItemController';
+import { SendOrderController } from './controllers/order/SendOrderController';
+import { ListOrderController } from './controllers/order/ListOrderController';
+import { DetailOrderController } from './controllers/order/DetailOrderController';
+import { FinishOrderController } from './controllers/order/FinishOrderController';
 
 import { isAuthenticated } from './middlewares/isAuthenticated'
 
@@ -35,7 +39,11 @@ router.post('/order', isAuthenticated, new CreateOrderController().handle)
 router.delete('/order', isAuthenticated, new RemoveOrderController().handle)
 
 router.post('/order/add', isAuthenticated, new AddItemController().handle)
-router.delete('/order/remove', isAuthenticated, new RemoveItemController().handle);
+router.delete('/order/remove', isAuthenticated, new RemoveItemController().handle)
+router.put('/order/send', isAuthenticated, new SendOrderController().handle)
+router.get('/orders', isAuthenticated, new ListOrderController().handle)
+router.get('/order/detail', isAuthenticated, new DetailOrderController().handle)
+router.put('/order/finish', isAuthenticated, new FinishOrderController().handle)
 
 export {router};
 
